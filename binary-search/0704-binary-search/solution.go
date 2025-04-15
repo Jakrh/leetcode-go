3f16ebb12b63b930606781292ce7e141309141ed
Jack (Lung-Hsuan) Hung <lhhungx@gmail.com>
2025-04-16 04:05:17 +0800

    Fix a bug in binary-search/0704-binary-search
    To avoid potential overflow in midpoint calculation of
    binary search.

--- binary-search/0704-binary-search/solution.go
+++ binary-search/0704-binary-search/solution.go
@@ -3,7 +3,7 @@ package solution
 func search(nums []int, target int) int {
 	l, m, r := 0, 0, len(nums)-1
 	for l <= r {
-		m = (l + r) / 2
+		m = l + (r-l)/2
 		if nums[m] < target {
 			l = m + 1
 		} else if target < nums[m] {
